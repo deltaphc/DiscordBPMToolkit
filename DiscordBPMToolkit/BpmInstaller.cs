ab7568c26dc921832b108d67cfe8c288b6d81dab
DeltaPHC <deltaphc@gmail.com>
2016-10-22 21:51:57 -0700

    Initial source code commit

---
+++ DiscordBPMToolkit/BpmInstaller.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace DiscordBPMToolkit
+{
+    class BpmInstaller
+    {
+        private readonly string DBTTempFolder =
+            Path.GetTempPath() + Path.DirectorySeparatorChar + "DiscordBPMToolkit" + Path.DirectorySeparatorChar;
+
+        private double progress = 0;
+
+        public BpmInstaller()
+        {
+            IsInstalling = false;
+        }
+
+        public async Task Install(bool usePTB)
+        {
+            IsInstalling = true;
+
+            if (!Directory.Exists(DBTTempFolder))
+                Directory.CreateDirectory(DBTTempFolder);
+
+            ProgressValue = 0;
+            InstallationStarted?.Invoke();
+
+            Initializing?.Invoke();
+            await Task.Run((Action)ExtractExeResources);
+            Initialized?.Invoke();
+            ProgressValue += 0.10;
+
+            BpmDownloading?.Invoke();
+            bool success = await DownloadLatestBpm();
+            if (!success)
+            {
+                ProgressValue = 0;
+                IsInstalling = false;
+                BpmDownloadError?.Invoke("Unable to download BPM! See Output for details.");
+                return;
+            }
+            BpmDownloaded?.Invoke();
+            ProgressValue += 0.10;
+
+            BpmExtracting?.Invoke();
+            await ExtractLatestBpm();
+            BpmExtracted?.Invoke();
+            ProgressValue += 0.10;
+
+            InstallScriptStarting?.Invoke();
+            success = await RunBpmInstallScript(usePTB);
+            if (!success)
+            {
+                ProgressValue = 0;
+                IsInstalling = false;
+                InstallScriptError?.Invoke("Encountered error(s). See Output for details.");
+                return;
+            }
+            InstallScriptFinished?.Invoke();
+            ProgressValue += 0.10;
+
+            CleanUpStarting?.Invoke();
+            await Task.Run((Action)CleanUpTempFiles);
+            CleanUpFinished?.Invoke();
+
+            ProgressValue = 1.0;
+            IsInstalling = false;
+            InstallationFinished?.Invoke();
+        }
+
+        private void ExtractExeResources()
+        {
+            File.WriteAllBytes(DBTTempFolder + "node.exe", Properties.Resources.Node);
+            File.WriteAllBytes(DBTTempFolder + "7za.exe", Properties.Resources.SevenZip);
+        }
+
+        private async Task<bool> DownloadLatestBpm()
+        {
+            ReleaseAsset latestAsset = null;
+            try
+            {
+                var github = new GitHubClient(new ProductHeaderValue("DiscordBPMToolkit"));
+                var latestRelease = await github.Repository.Release.GetLatest("ByzantineFailure", "BPM-for-Discord");
+                foreach (var asset in latestRelease.Assets)
+                {
+                    if (asset.Name.EndsWith(".7z"))
+                    {
+                        latestAsset = asset;
+                        break;
+                    }
+                }
+                if (latestAsset == null) return false;
+            }
+            catch
+            {
+                return false;
+            }
+
+            var archivePath = DBTTempFolder + "BPM-for-Discord-latest.7z";
+            using (var wc = new WebClient())
+            {
+                wc.DownloadFile(latestAsset.BrowserDownloadUrl, archivePath);
+            }
+            if (!File.Exists(archivePath)) return false;
+
+            return true;
+        }
+
+        private async Task ExtractLatestBpm()
+        {
+            var sevenZipPath = DBTTempFolder + "7za.exe";
+            var archivePath = DBTTempFolder + "BPM-for-Discord-latest.7z";
+            var pi = new ProcessStartInfo(sevenZipPath, "x -bb1 -o\"" + DBTTempFolder + "\" -- \"" + archivePath + "\"");
+            pi.CreateNoWindow = true;
+            pi.WindowStyle = ProcessWindowStyle.Hidden;
+            pi.UseShellExecute = false;
+            pi.RedirectStandardOutput = true;
+            using (var p = Process.Start(pi))
+            {
+                string output = await p.StandardOutput.ReadLineAsync();
+                while (output != null)
+                {
+                    ProgressValue += (0.2 / 2500); // 20% progress taken by this step divided by approximate output lines generated
+                    output = await p.StandardOutput.ReadLineAsync();
+                }
+                p.WaitForExit();
+            }
+        }
+
+        private async Task<bool> RunBpmInstallScript(bool usePTB)
+        {
+            bool success = true;
+            var workingFolder = DBTTempFolder + "discord\\";
+            var pi = new ProcessStartInfo("cmd.exe", "/C \"\"" + DBTTempFolder + "node.exe\" \"" + workingFolder + "index.js\" \"" + DBTTempFolder + "discord\"");
+            pi.Arguments += usePTB ? " --isPTB" : "";
+            pi.Arguments += " 2>&1\""; //Redirects stderr to stdout
+            pi.WorkingDirectory = workingFolder;
+            pi.CreateNoWindow = true;
+            pi.WindowStyle = ProcessWindowStyle.Hidden;
+            pi.UseShellExecute = false;
+            pi.RedirectStandardOutput = true;
+            pi.RedirectStandardInput = true;
+            using (var p = Process.Start(pi))
+            {
+                string output = await p.StandardOutput.ReadLineAsync();
+                while (output != null)
+                {
+                    if (output.Contains("Error"))
+                        success = false;
+                    InstallScriptOutput?.Invoke(output);
+                    ProgressValue += 0.01;
+                    output = await p.StandardOutput.ReadLineAsync();
+                }
+
+                p.StandardInput.WriteLine(); //Simulate pressing enter in case it has a "press any key" prompt
+                p.WaitForExit();
+            }
+            return success;
+        }
+
+        private void CleanUpTempFiles()
+        {
+            Directory.Delete(DBTTempFolder + "discord", true);
+            foreach (string filename in Directory.EnumerateFiles(DBTTempFolder))
+            {
+                File.Delete(filename);
+            }
+        }
+
+        public event Action InstallationStarted;
+        public event Action Initializing;
+        public event Action Initialized;
+        public event Action BpmDownloading;
+        public event Action<string> BpmDownloadError;
+        public event Action BpmDownloaded;
+        public event Action BpmExtracting;
+        public event Action BpmExtracted;
+        public event Action InstallScriptStarting;
+        public event Action<string> InstallScriptOutput;
+        public event Action<string> InstallScriptError;
+        public event Action InstallScriptFinished;
+        public event Action CleanUpStarting;
+        public event Action CleanUpFinished;
+        public event Action InstallationFinished;
+        public event Action<double, double> ProgressChanged;
+
+        public bool IsInstalling { get; private set; }
+        public double ProgressValue
+        {
+            get { return progress; }
+            private set
+            {
+                double oldValue = progress;
+                progress = value;
+                ProgressChanged?.Invoke(oldValue, progress);
+            }
+        }
+        public string TempFolder => DBTTempFolder;
+    }
+}
---
+++ DiscordBPMToolkit/MainWindow.xaml.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shell;
+using System.Windows.Threading;
+
+namespace DiscordBPMToolkit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string InstallingPrefix = "[installing] - ";
+
+        private BpmInstaller installer;
+        private bool isDiscordRunning = false;
+        private double collapsedHeight;
+        private DispatcherTimer timer;
+
+        private Stopwatch stopwatch;
+        private long startTime = 0;
+        private long endTime = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            TaskbarItemInfo = new TaskbarItemInfo();
+            collapsedHeight = Height;
+
+            // For updating the "Launch/Restart Discord" button
+            timer = new DispatcherTimer(DispatcherPriority.ApplicationIdle, Dispatcher);
+            timer.Interval = TimeSpan.FromSeconds(1.0);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            stopwatch = new Stopwatch();
+
+            string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string discordInstallPath = localAppData + "Discord";
+            string discordPTBInstallPath = localAppData + "DiscordPTB";
+            if (Directory.Exists(discordInstallPath) && Directory.Exists(discordPTBInstallPath))
+                isPTB.Visibility = Visibility.Visible;
+            else if (Directory.Exists(discordPTBInstallPath) && !Directory.Exists(discordInstallPath))
+                isPTB.IsChecked = true;
+            else
+                isPTB.IsChecked = false;
+
+            installer = new BpmInstaller();
+            installer.ProgressChanged += Installer_ProgressChanged;
+            installer.InstallationStarted += Installer_InstallationStarted;
+            installer.Initializing += Installer_Initializing;
+            installer.Initialized += Installer_Initialized;
+            installer.BpmDownloading += Installer_BpmDownloading;
+            installer.BpmDownloadError += Installer_BpmDownloadError;
+            installer.BpmDownloaded += Installer_BpmDownloaded;
+            installer.BpmExtracting += Installer_BpmExtracting;
+            installer.BpmExtracted += Installer_BpmExtracted;
+            installer.InstallScriptStarting += Installer_InstallScriptStarting;
+            installer.InstallScriptOutput += Installer_InstallScriptOutput;
+            installer.InstallScriptError += Installer_InstallScriptError;
+            installer.InstallScriptFinished += Installer_InstallScriptFinished;
+            installer.CleanUpStarting += Installer_CleanUpStarting;
+            installer.CleanUpFinished += Installer_CleanUpFinished;
+            installer.InstallationFinished += Installer_InstallationFinished;
+
+            installLog.Text =
+                $"Discord BPM Toolkit {Properties.Resources.Version}\nby DeltaPHC\n\n";
+        }
+
+        private void Installer_ProgressChanged(double oldValue, double newValue)
+        {
+            installProgress.Value = newValue;
+        }
+
+        private void Installer_CleanUpFinished()
+        {
+            endTime = stopwatch.ElapsedMilliseconds;
+            installLog.AppendText($"done in {endTime - startTime}ms\n");
+        }
+
+        private void Installer_CleanUpStarting()
+        {
+            startTime = stopwatch.ElapsedMilliseconds;
+            installLog.AppendText("Cleaning up temporary files... ");
+            installStatus.Text = "Cleaning up";
+        }
+
+        private void Installer_InstallScriptFinished()
+        {
+            endTime = stopwatch.ElapsedMilliseconds;
+            installLog.AppendText($"Install script done in {endTime - startTime}ms\n");
+        }
+
+        private void Installer_InstallScriptError(string errorText)
+        {
+            installStatus.Text = errorText;
+            outputExpander.IsExpanded = true;
+            installButton.IsEnabled = true;
+            launchButton.IsEnabled = true;
+            aboutButton.IsEnabled = true;
+            Title = Title.Remove(0, InstallingPrefix.Length);
+            TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Error;
+        }
+
+        private void Installer_InstallScriptOutput(string output)
+        {
+            installLog.AppendText(output + "\n");
+        }
+
+        private void Installer_InstallScriptStarting()
+        {
+            startTime = stopwatch.ElapsedMilliseconds;
+            installLog.AppendText("Running install script...\n");
+            installStatus.Text = "Installing BPM. This may take a minute or two";
+        }
+
+        private void Installer_BpmExtracted()
+        {
+            endTime = stopwatch.ElapsedMilliseconds;
+            installLog.AppendText($"done in {endTime - startTime}ms\n");
+        }
+
+        private void Installer_BpmExtracting()
+        {
+            startTime = stopwatch.ElapsedMilliseconds;
+            installLog.AppendText("Extracting BPM... ");
+            installStatus.Text = "Extracting BPM";
+        }
+
+        private void Installer_BpmDownloaded()
+        {
+            endTime = stopwatch.ElapsedMilliseconds;
+            installLog.AppendText($"done in {endTime - startTime}ms\n");
+        }
+
+        private void Installer_BpmDownloadError(string errorMessage)
+        {
+            installLog.AppendText("\n\nThere was a problem downloading BPM. Make sure your internet connection is working. If you have internet, then GitHub may be down, or you may be out of storage space.\n");
+            installStatus.Text = errorMessage;
+            outputExpander.IsExpanded = true;
+            installButton.IsEnabled = true;
+            launchButton.IsEnabled = true;
+            aboutButton.IsEnabled = true;
+            Title = Title.Remove(0, InstallingPrefix.Length);
+            TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Error;
+        }
+
+        private void Installer_BpmDownloading()
+        {
+            startTime = stopwatch.ElapsedMilliseconds;
+            installLog.AppendText("Downloading latest BPM for Discord... ");
+            installStatus.Text = "Downloading latest BPM for Discord";
+        }
+
+        private void Installer_Initialized()
+        {
+            endTime = stopwatch.ElapsedMilliseconds;
+            installLog.AppendText($"done in {endTime - startTime}ms\n");
+        }
+
+        private void Installer_Initializing()
+        {
+            startTime = stopwatch.ElapsedMilliseconds;
+            installLog.AppendText("Initializing... ");
+            installStatus.Text = "Initializing. One moment, please";
+        }
+
+        private void Installer_InstallationFinished()
+        {
+            stopwatch.Stop();
+            installLog.AppendText($"Installation finished in {stopwatch.ElapsedMilliseconds}ms\n\n");
+            installLog.AppendText("\nBPM for Discord is now installed!\n");
+            installStatus.Text = "Done! Be sure to restart Discord.";
+            installButton.IsEnabled = true;
+            launchButton.IsEnabled = true;
+            aboutButton.IsEnabled = true;
+            Title = Title.Remove(0, InstallingPrefix.Length);
+        }
+
+        private void Installer_InstallationStarted()
+        {
+            stopwatch.Reset();
+            stopwatch.Start();
+            Title = InstallingPrefix + Title;
+            installButton.IsEnabled = false;
+            launchButton.IsEnabled = false;
+            aboutButton.IsEnabled = false;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var discordProcesses = Process.GetProcessesByName("Discord");
+            isDiscordRunning = (discordProcesses.Length > 0);
+
+            if (isDiscordRunning)
+                launchButton.Content = "Restart Discord";
+            else
+                launchButton.Content = "Launch Discord";
+
+            if (!installer.IsInstalling)
+                launchButton.IsEnabled = true;
+            timer.Interval = TimeSpan.FromSeconds(1.0);
+        }
+
+        private async void installButton_Click(object sender, RoutedEventArgs e)
+        {
+            await installer.Install((bool)isPTB.IsChecked);
+        }
+
+        private void installProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TaskbarItemInfo.ProgressValue = e.NewValue;
+            if (e.NewValue == 1.0)
+                TaskbarItemInfo.ProgressState = TaskbarItemProgressState.None;
+            else
+                TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Normal;
+        }
+
+        private void outputExpander_Expanded(object sender, RoutedEventArgs e)
+        {
+            outputExpander.Header = "Hide Output";
+            collapsedHeight = Height;
+            if (Height < 450.0) Height = 450.0;
+        }
+
+        private void outputExpander_Collapsed(object sender, RoutedEventArgs e)
+        {
+            outputExpander.Header = "Show Output";
+            Height = collapsedHeight;
+        }
+
+        private void LaunchDiscord()
+        {
+            string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + System.IO.Path.DirectorySeparatorChar;
+            string discordBase = localAppData + ((bool)isPTB.IsChecked ? "DiscordPTB" : "Discord") + System.IO.Path.DirectorySeparatorChar;
+            string updateExePath = discordBase + "Update.exe";
+            Process.Start(updateExePath, "--processStart Discord.exe");
+        }
+
+        private void launchButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isDiscordRunning)
+            {
+                LaunchDiscord();
+            }
+            else
+            {
+                launchButton.IsEnabled = false;
+                timer.Stop();
+                timer.Interval = TimeSpan.FromSeconds(5.0);
+                timer.Start();
+
+                var discordProcesses = Process.GetProcessesByName("Discord");
+                foreach (var process in discordProcesses)
+                {
+                    if (!process.HasExited)
+                    {
+                        process.Kill();
+                        process.WaitForExit();
+                    }
+                    process.Close();
+                }
+                LaunchDiscord();
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = installer.IsInstalling;
+        }
+
+        private void installLog_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            installLog.ScrollToEnd();
+        }
+
+        private void aboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            (new AboutWindow()).ShowDialog();
+        }
+    }
+}
